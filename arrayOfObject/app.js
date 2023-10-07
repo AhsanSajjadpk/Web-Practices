7f8cbd61b4afc1abc055343e3cca1f31dabce81e
Ahsan Sajjad <princeahsan0608@gmail.com>
2023-10-07 12:46:01 -0700

    arrayOfObject, Filter(), Foreach() and Map() practice

---
+++ arrayOfObject/app.js
@@ -0,0 +1,66 @@
+
+// array of object
+
+// var aoo = [
+//     {
+//         id : '01',
+//         Name : "Ahsan",
+//         Department : "BSE"
+//     },
+//     {
+//         id : '02',
+//         Name : "Bilal",
+//         Department : "BCE"
+//     },
+//     {
+//         id : '03',
+//         Name : "Haseeb",
+//         Department : "BBA"
+//     }
+//     ]
+
+//     for(var i=0; i< aoo.length; i++){
+
+//         for (var key in aoo[i]){
+//             document.write(key , " : ",aoo[i][key] ,"<br>")
+//         }
+//         if(i< aoo.length-1){
+//         document.write("<br><br> ----------- <br>")
+//     }
+//     }
+
+
+//     ---------------- Filter
+
+// var scrores =[15,32,35,50,92,15,85,65,12,74,21,100]
+
+// var filterarray=scrores.filter(function(values){
+//     return values>=50
+// })
+
+// document.write(filterarray)
+
+
+
+// Foreach
+
+
+// var names= ['ali','usama','nadeem','ali','usama','nadeem']
+
+// names.forEach(function(values){
+//    document.write(values,"<br>")
+// })
+
+
+// // ---------------------MAp
+
+
+// var names= ['ali','usama','nadeem','ali','usama','nadeem']
+
+// names.map(function(values){
+//    document.write(values,"<br>")
+// })
+
+
+
+
